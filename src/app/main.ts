c5ba33cec33c06ba7fe4c5e0fdcc5ce7d403117c
Alex Eden <alexandereden91@gmail.com>
2018-12-10 02:03:58 -0600

    Test the springs

--- src/app/main.ts
+++ src/app/main.ts
@@ -5,6 +5,38 @@ const canvas = document.createElement('canvas');
 const sandbox = new Sandbox(canvas).setHeight(window.innerHeight).setWidth(window.innerWidth);
 document.body.appendChild(canvas);
 
+const h1 = canvas.height / 2;
+const h2 = h1 + 100;
+const dx = canvas.width / 10;
+const w = canvas.width;
+const wave = new Shape();
+const wp1 = wave.addFixedPoint(dx, h1);
+const wp6 = wave.addFixedPoint(dx * 6, h1);
+const wp7 = wave.addFixedPoint(dx * 2, h2);
+const wp8 = wave.addFixedPoint(dx * 3, h2);
+const wp9 = wave.addFixedPoint(dx * 4, h2);
+const wp10 = wave.addFixedPoint(dx * 5, h2);
+const wp2 = wave.addPoint(dx * 2, h1);
+const wp3 = wave.addPoint(dx * 3, h1);
+const wp4 = wave.addPoint(dx * 4, h1);
+const wp5 = wave.addPoint(dx * 5, h1);
+
+const kh = 0.5;
+const kv = 0.5;
+wave
+    .spring(wp1, wp2, kh)
+    .spring(wp2, wp3, kh)
+    .spring(wp3, wp4, kh)
+    .spring(wp4, wp5, kh)
+    .spring(wp5, wp6, kh)
+    .spring(wp2, wp7, kv)
+    .spring(wp3, wp8, kv)
+    .spring(wp4, wp9, kv)
+    .spring(wp5, wp10, kv);
+
+
+sandbox.addShape(wave);
+
 const crane = new Shape();
 const p1 = crane.addPoint(90, 40);
 const p2 = crane.addPoint(160, 40);
@@ -47,7 +79,7 @@ crane
     .connect(p8, p9)
     .connect(p2, p11)
     .connect(p4, p11)
-    .spring(p11, p12, 0.01)
+    .spring(p11, p12, 0.1)
     .connect(p12, p13)
     .connect(p12, p14)
     .connect(p13, p14);
